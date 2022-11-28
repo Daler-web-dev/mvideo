b73a53553992d230ca47adc85e0dadc8a16baf94
Begzod <dusmamatov46@gmail.com>
2022-11-28 21:02:24 +0500

    fixed

--- scriptForproductpage.js
+++ scriptForproductpage.js
@@ -1,27 +1,35 @@
-let id = localStorage.getItem("id")
-let url = "http://localhost:7777/goods"
+let id = localStorage.getItem("id");
+let url = "http://localhost:7777/goods";
 
-let title = document.querySelector('.title')
-let productImg = document.querySelector('.productImg') 
-let priceTobuy = document.querySelector('.priceTobuy')
+let title = document.querySelector(".title");
+let productImg = document.querySelector(".productImg");
+let priceTobuy = document.querySelector(".priceTobuy");
+let bnt_to_cart = document.querySelector(".toCart");
 
-axios.get(url+"/"+id)
-    .then(res => reloadforProduct(res.data))
+let toCartId = JSON.parse(localStorage.getItem("liked")) || [];
 
-
+axios.get(url + "/" + id).then((res) => reloadforProduct(res.data));
 
 function reloadforProduct(arr) {
-    title.innerHTML = arr.title
+  title.innerHTML = arr.title;
 
-    productImg.src = arr.media[0]
+  productImg.src = arr.media[0];
 
-    if (arr.salePercentage = 0) {
-        priceTobuy.innerHTML = arr.price
+  if ((arr.salePercentage = 0)) {
+    priceTobuy.innerHTML = arr.price;
+  } else {
+    let discount = arr.price - (arr.salePercentage * arr.price) / 100;
+    priceTobuy.innerHTML = Math.round(discount);
+  }
+
+  bnt_to_cart.onclick = () => {
+    if (toCartId.includes(arr.id)) {
+      toCartId = toCartId.filter((el) => el !== arr.id);
+      localStorage.setItem("liked", JSON.stringify(toCartId));
     } else {
-        let discount = arr.price - (arr.salePercentage * arr.price / 100)
-        priceTobuy.innerHTML = Math.round(discount)
-
+      toCartId.push(arr.id);
+      localStorage.setItem("liked", JSON.stringify(toCartId));
     }
-
-    
+    console.log(toCartId);
+  };
 }
